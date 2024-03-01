bd0a047380c991db7c7431c45bf38374092be549
PavanButke <butkepavan55@gmail.com>
2024-03-01 22:34:26 +0530

    Project Initialized

---
+++ src/main/java/com/myntra/SolvingProblemApplication.java
@@ -0,0 +1,13 @@
+package com.myntra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolvingProblemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolvingProblemApplication.class, args);
+	}
+
+}
